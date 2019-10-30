e754b06097836dbd9909f7b214f0019b370d7012
Kyubin <kyubin.lee@gmail.com>
2019-10-30 18:29:40 +0900

    ✨Add spaces, notes feature(creaate & fetch)

--- models/space.js
+++ models/space.js
@@ -1,12 +1,20 @@
 const conn = require('../database/connection');
 
 module.exports = {
-    get: (args, callback) => {  
-
+    get: ({userId}, callback) => {  
+      console.log(`[Model.Space][Param:${userId}]`);
+      const sql = `SELECT id, url, name FROM spaces, shared 
+                   WHERE spaces.id = shared.space_id AND shared.user_id = ?;`;
+      const arg = [userId];
+      console.log(`[Model.Space][SQL:${sql}]`);
+      conn.query(sql, arg, (err, results) => {
+        if (err) callback(err, null);
+        else callback(null, results);
+      });  
     },
     getOne: ({spaceId}, callback) => {
       console.log(`[Model.Space][Param:${spaceId}]`);
-      const sql = 'SELECT id, url, name FROM spaces WHERE id = ?;';
+      const sql = `SELECT id, url, name FROM spaces WHERE id = ?;`;
       const arg = [spaceId];
       console.log(`[Model.Space][SQL:${sql}]`);
       conn.query(sql, arg, (err, results) => {
@@ -14,13 +22,24 @@ module.exports = {
         else callback(null, results);
       });
     },
-    post: (args, callback) => {
-
+    post: ({url, name, userId}, callback) => {
+      console.log(`[Model.Space][Param:${url}, ${name}, ${userId}]`);
+      const sql = `INSERT INTO spaces (url, name) VALUES (?, ?)`;
+      const arg = [url, name];
+      console.log(`[Model.Space][SQL:${sql}]`);
+      conn.query(sql, arg, (err, results) => {
+        if (err) callback(err, null);
+        else {
+          const spaceId = results.insertId;
+          const sql = `INSERT INTO shared VALUES (?, ?);`;
+          const arg = [userId, spaceId];
+          conn.query(sql, arg, (err, results) => {
+            if (err) callback(err, null);
+            else callback(null, spaceId);
+          });
+        }
+      });
     },
-    put: (args, callback) => {
-
-    },
-    delete: (args, callback) => {
-
-    }
+    put: (args, callback) => {},
+    delete: (args, callback) => {}
   }
