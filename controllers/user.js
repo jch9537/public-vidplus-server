4488297c44521ad77bc71ef82c9c52d2852ed555
jch9537 <jch9537@gmail.com>
2019-10-30 20:00:11 +0900

    complete signout

--- controllers/user.js
+++ controllers/user.js
@@ -3,12 +3,15 @@ const userModel = require('../models/user');
 
 module.exports = {
   signup: (req, res) => {
-    console.log(`[Ctrl.User][Request:${JSON.stringify(req.body)}]`);
+    const data = req.body
+    // console.log(`[Ctrl.User][Request:${JSON.stringify(req.body)}]`);
     // Controller 핸들링
     const args = {
-      email: req.body.email,
-      password: req.body.password,
-      name: req.body.name
+      email: data.email,
+      password: crypto.createHmac('sha512', process.env.SRV_CRYPTO_SALT) //salt부분 맞는지 확인
+                      .update(data.password)
+                      .digest('base64'),
+      name: data.name
     };
     // Model 호출
     userModel.signup(args, (err, data) => {
@@ -17,10 +20,40 @@ module.exports = {
     });
   },
   signin: (req, res) => {
-    res.status(200).send('Sign In');
+    let sess = req.session;
+    const data = req.body;
+    const args = {
+      email: data.email,
+      password: crypto.createHmac('sha512', process.env.SRV_CRYPTO_SALT) 
+                      .update(data.password)
+                      .digest('base64'),
+    }
+    userModel.signin(args, (err, data) => {
+      if(err) {
+        res.status(500).send(err);
+        // 번호는 규칙에 맞게 에러 타입에 맞는 메세지 보내기
+        // console.log()서버에서 에러메세지
+        // res.status.send로 에러보내기
+      } else {
+        console.log('데이터 : ', data)
+        sess.userid = data[0].id;
+        res.status(200).send('Log in.')
+      }
+    });
   },
   signout: (req, res) => {
-    res.status(200).send('Sign Out');
+    const sess  = req.session;
+    if(sess.userid) {
+      req.session.destroy(err => {
+        if(err) {
+          res.status(500)
+        } else {
+          res.redirect(200, '/');
+        }
+      })
+    } else {
+      res.redirect(200, '/');
+    }
   },
   get: (req, res) => {
     res.status(200).send('Get User');
--- models/user.js
+++ models/user.js
@@ -2,21 +2,28 @@ const conn = require('../database/connection');
 
 module.exports = {
     signup: ({email, password, name}, callback) => {  
-      console.log(`[Model.User][Param:{${email}, ${password}, ${name}}]`);
+      // console.log(`[Model.User][Param:{${email}, ${password}, ${name}}]`);
       const sql = 'INSERT INTO users (email, password, name) VALUES (?, ?, ?);';
       const arg = [email, password, name];
-      console.log(`[Model.User][SQL:${sql}]`);
+      // console.log(`[Model.User][SQL:${sql}]`);
       conn.query(sql, arg, (err, results) => {
         if (err) callback(err, null);
         else callback(null, results);
       });
     },
-    signin: (args, callback) => {
+    //구분해서 에러반환 있으면 사용중인 이메일이라고 알려주고 에러메세지로 구분 401이메일 중복 등...
+    //쿼리문으로 select로 먼저 날리고 있으면 중복에러메세지 띄우고 없으면 등록
+    signin: ({email, password}, callback) => {
+      const sql = `SELECT * FROM users WHERE email='${email}' AND password='${password}'`
+      conn.query(sql, (err, results) => {
+        if(err) callback(err, null);
+        else(callback(null, results))
+      })
 
     },
-    signout: (args, callback) => {
-
-    },
+    // signout: (args, callback) => {
+    //  sessionId로 해서 필요없을 것 같음
+    // },
     get: (args, callback) => {
 
     },
