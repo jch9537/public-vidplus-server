12c60a82fce26f71ee17bc0a5dc89ada8c3cbee1
Kyubin-Lee <kyubin.lee@gmail.com>
2019-11-04 23:17:29 +0900

    oauth wip

--- controllers/oauth.js
+++ controllers/oauth.js
@@ -3,6 +3,6 @@ const conn = require('../database/connection');
 module.exports = {
   signinOrSignup: (profile, callback) => {
     console.log(JSON.stringify(profile));
-    callback(null, null);
+    callback(null, profile);
   }
 }
