20173079918f0fe0d69ade04184319d42aa4347a
Kyubin <kyubin.lee@gmail.com>
2019-11-04 18:36:50 +0900

    oauth wip

--- app.js
+++ app.js
@@ -24,6 +24,32 @@ app.use(
   })
 );
 
+
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
+app.use(passport.initialize());
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    return done(null, profile); // done() 실행하여야 정상종료
+  }
+));
+
+passport.serializeUser(function(profile, done) {
+  console.log(profile+'!!!');
+  done(null, profile);
+});
+  
+passport.deserializeUser(function(profile, done) {
+    console.log(profile+'???');
+  done(null, profile);
+});
+
+
 app.use(express.json());
 app.use("/", routes);
 
