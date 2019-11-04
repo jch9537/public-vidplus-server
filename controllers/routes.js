e59bff971faf8d9693b7eea88975b6c25c73a1ba
Kyubin-Lee <kyubin.lee@gmail.com>
2019-11-05 00:01:07 +0900

    oauth wip

--- controllers/routes.js
+++ controllers/routes.js
@@ -8,7 +8,7 @@ const noteCtrl = require('./note');
 routes.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login', 'email'] }));
 routes.get('/auth/callback', 
   passport.authenticate('google', { session: false }), (req, res) => res.redirect(`${process.env.CLNT_ORIGIN}/auth/sigininOrSignup?email=${process.env.CLNT_ORIGIN}`));
-routes.get('/auth/signinOrSignup', authCtrl.signinOrSignout);
+routes.get('/auth/signinOrSignup', authCtrl.signinOrSignup);
 
 routes.post('/user/signup', userCtrl.signup);
 routes.post('/user/signin', userCtrl.signin);
