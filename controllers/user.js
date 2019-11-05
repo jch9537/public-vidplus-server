f745ac48eb4de61b8ffd8e0822d47925bcb7fe0d
jch9537 <jch9537@gmail.com>
2019-11-05 19:30:44 +0900

    ✨ Edit Users

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,7 @@ const userModel = require("../models/user");
 module.exports = {
   signup: (req, res) => {
     const data = req.body;
-    // console.log('리퀘스트 바디 : ', req.body)
+
     if(!(data.email && data.password && data.name)){
       res.status(400).send({err: {status: 400, message: 'body를 다음과 같이 수정해주세요: {email, password, name}'}});
     } else {
@@ -27,7 +27,7 @@ module.exports = {
   signin: (req, res) => {
     const data = req.body;
     const sess = req.session;
-    // console.log('리퀘스트 바디 : ', data);
+
     if(!(data.email && data.password)){
       res.status(400).send({err: {status: 400, message: 'body를 다음과 같이 수정해주세요: {email, password, name}'}});
     } else {
@@ -39,13 +39,10 @@ module.exports = {
           .digest("base64")
       };
       userModel.signin(args, (err, results) => {
-        console.log('사인인 리절트 ', results)
-        console.log('사인인 에러 ', err)
-        if(err === 'No match Email') return res.status(409).send({err: {status: 409, message: "일치하는 회원 정보가 존재하지 않습니다."}})
-        if(err === 'No match password') return res.status(401).send({err: {status: 401, message: "비밀번호가 일치하지 않습니다."}});
         if (err) return res.status(500).send({err: {status: 500, message: "Sign in 실패"}});
         else {
-          if(!results.length) return res.status(401).send({err: {status: 401, message: "비밀번호가 일치하지 않습니다."}});
+          if(results === 'No match Email') return res.status(409).send({err: {status: 409, message: "일치하는 회원 정보가 존재하지 않습니다."}});
+          else if(results === 'No match password') return res.status(401).send({err: {status: 401, message: "비밀번호가 일치하지 않습니다."}});
           sess.userid = results[0].id;
           res.status(200).send({ message: "Sign-in successful." });
         }
@@ -54,7 +51,6 @@ module.exports = {
   },
 
   signout: (req, res) => {
-    // req.session.userid = 5;
     if (!req.session.userid) {
       res.status(406).send({err: {status: 406, message: "Sign in 상태가 아닙니다."}})
     } else {
@@ -66,7 +62,6 @@ module.exports = {
   },
 
   get: (req, res) => {
-    // req.session.userid = 5;
     const args = req.session.userid;
 
     if (!req.session.userid) {
@@ -75,7 +70,6 @@ module.exports = {
       userModel.get(args, (err, results) => {
         if (err) res.status(500).send({err: {status: 500, message: "회원정보 가져오기 실패"}});
         else {
-          // console.log("리절트 :", results[0].email, results[0].name);
           res.status(200).send({email: results[0].email, name: results[0].name});
         }
       });
@@ -83,8 +77,6 @@ module.exports = {
   },
 
   put: (req, res) => {
-    // req.session.userid = 11;
-    // console.log("리퀘스트 바디 : ", req.body);
     if(!req.session.userid) {
       res.status(401).send({err: {status: 401, message: "회원정보를 가져오기 위해 sign in이 필요합니다."}})
     } 
@@ -100,10 +92,8 @@ module.exports = {
         id: req.session.userid
       };
         userModel.put(args, (err, results) => {
-          // console.log('에러 확인 : ', err)
           if (err) res.status(500).send({err: {status: 500, message: "회원정보 수정 실패"}});
           else {
-            // console.log('풋리절트 에러', results)
             if(!results.length) return res.status(409).send({err: {status: 400, message: "다른 유저가 사용 중인 email 입니다."}}); 
             const { email, name } = results[0];
             res.status(200).send({email, name, status: 200, message: '회원정보 수정 완료'});
@@ -113,7 +103,7 @@ module.exports = {
   },
   delete: (req, res) => {
     const sess = req.session;
-    sess.userid = 5;
+ 
     if (!sess.userid) {
       res.status(401).send({err:{status:401, message: "Unauthorized - sign in이 필요합니다."}});
     } 
--- models/user.js
+++ models/user.js
@@ -2,54 +2,57 @@ const conn = require('../database/connection');
 
 module.exports = {
     signup: ({email, password, name}, callback) => { 
-      // console.log(`[Model.User][Param:{${email}, ${password}, ${name}}]`);
       let sql = 'INSERT INTO users (email, password, name) VALUES (?, ?, ?);';
       const arg = [email, password, name];
-      // console.log(`[Model.User][SQL:${sql}]`);
       conn.query(sql, arg, (err, results) => {
         if (err) callback(err, null);
         else callback(null, results);
       });
     },
+
     signin: ({email, password}, callback) => {
       let sql = `SELECT * FROM users WHERE email='${email}'`
       conn.query(sql, (err, results) => {
-        if(err) callback('No match Email', null)
+        if(err) callback(err, null);
         else {
-          sql = `SELECT * FROM users WHERE email='${email}' AND password='${password}'`
-          conn.query(sql, (err, results) => {
-            if(err) callback('No match password', null)
-            else(callback(null, results))
-          })
+          if(!results.length) callback(null, 'No match Email')
+          else {
+            sql = `SELECT * FROM users WHERE email='${email}' AND password='${password}'`
+            conn.query(sql, (err, results) => {
+              if(err) callback(err, null);
+              else{
+                if(!results.length) callback(null, 'No match password')
+                else callback(null, results);
+              }
+            })        
+          }
         }
       })
     },
+
     get: (id, callback) => {
       let sql = `SELECT * FROM users WHERE id='${id}'`;
-      // console.log('에스큐엘 :', sql)
       conn.query(sql, (err, result) => {
         if(err){
           callback(err, null);
         } else callback(null, result);
       });
     },
+
     put: ({email, password, name, id}, callback) => {
-      // console.log('아규먼트 :', email,':', password,':', name, ':',id)
       let sql = `UPDATE users SET email='${email}', password='${password}', name='${name}' WHERE id='${id}';`
-      // console.log('에스큐엘 ', sql)
       conn.query(sql, (err, results) => {
-        console.log('업데이트 에러', err)
         if(err) callback(err, null);
         else {
           sql = `SELECT * FROM users WHERE id='${id}';`
           conn.query(sql, (err, results) => {
-            console.log('셀렉트 에러 : ', err)
             if(err) callback(err, null);
             else callback(null, results);
           })
         }
       })
     },
+    
     delete: (id, callback) => {
       let sql = `DELETE FROM users WHERE id='${id}';`
       conn.query(sql, (err, results) => {
