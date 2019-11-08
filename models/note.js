562aafe27b076bfa0f0c10c7df3aa6f295972613
Andrew Chung <andrewchung2022@u.northwestern.edu>
2019-11-08 09:52:02 +0900

    🐛 note models PUT - check if user owns the note

--- models/note.js
+++ models/note.js
@@ -1,45 +1,53 @@
-const conn = require('../database/connection');
+const conn = require("../database/connection");
 
 module.exports = {
-    get: ({spaceId, userId}, callback) => {  
-      const sql = `SELECT id, space_id, timestamp, content FROM notes 
+  get: ({ spaceId, userId }, callback) => {
+    const sql = `SELECT id, space_id, timestamp, content FROM notes 
                    WHERE space_id = (SELECT space_id FROM shared WHERE space_id = ? AND user_id = ?);`;
-      const arg = [spaceId, userId];
-      conn.query(sql, arg, (err, results) => {
-        if (err) return callback(err, null);
-        return callback(null, results);
+    const arg = [spaceId, userId];
+    conn.query(sql, arg, (err, results) => {
+      if (err) return callback(err, null);
+      return callback(null, results);
+    });
+  },
+  post: ({ spaceId, timestamp, content }, callback) => {
+    const sql = `INSERT INTO notes (space_id, timestamp, content) VALUES (?, ?, ?);`;
+    const arg = [spaceId, timestamp, content];
+    conn.query(sql, arg, (err, results) => {
+      if (err) return callback(err, null);
+      return callback(null, results);
+    });
+  },
+  put: ({ userId, id, spaceId, timestamp, content }, callback) => {
+    let sql = `SELECT user_id FROM shared 
+                 WHERE user_id = ? AND space_id = ?;`;
+    let arg = [userId, spaceId];
+    conn.query(sql, arg, (e1, r1) => {
+      if (e1) return callback(e1, null);
+      if (!r1.length) return callback(0, null);
+      sql = `UPDATE notes SET timestamp = ?, content = ?, space_id = ? WHERE id = ?;`;
+      arg = [timestamp, content, spaceId, id];
+      conn.query(sql, arg, (e2, r2) => {
+        if (e2) return callback(e2, null);
+        return callback(null, r2);
       });
-    },
-    post: ({spaceId, timestamp, content}, callback) => {
-      const sql = `INSERT INTO notes (space_id, timestamp, content) VALUES (?, ?, ?);`;
-      const arg = [spaceId, timestamp, content];
-      conn.query(sql, arg, (err, results) => {
-        if (err) return callback(err, null);
-        return callback(null, results);
-      });
-    },
-    put: ({id, timestamp, content}, callback) => {
-      const sql = `UPDATE notes SET timestamp = ?, content = ? WHERE id = ?;`;
-      const arg = [timestamp, content, id];
-      conn.query(sql, arg, (err, results) => {
-        if (err) return callback(err, null);
-        return callback(null, results);
-      });
-    },
-    delete: ({id, userId}, callback) => {
-      let sql = `SELECT user_id FROM shared 
+      return null;
+    });
+  },
+  delete: ({ id, userId }, callback) => {
+    let sql = `SELECT user_id FROM shared 
                  WHERE user_id = ? AND space_id = (SELECT space_id FROM notes WHERE id = ?);`;
-      let arg = [userId, id];
-      conn.query(sql, arg, (e1, r1) => {
-        if (e1) return callback(e1, null);
-        if (!r1.length) return callback(0, null);
-        sql = 'DELETE FROM notes WHERE id = ?;';
-        arg = [id];
-        conn.query(sql, arg, (e2, r2) => {
-          if (e2) return callback(e2, null);
-          return callback(null, r2);
-        });
-        return null;
+    let arg = [userId, id];
+    conn.query(sql, arg, (e1, r1) => {
+      if (e1) return callback(e1, null);
+      if (!r1.length) return callback(0, null);
+      sql = "DELETE FROM notes WHERE id = ?;";
+      arg = [id];
+      conn.query(sql, arg, (e2, r2) => {
+        if (e2) return callback(e2, null);
+        return callback(null, r2);
       });
-    }
+      return null;
+    });
   }
+};
