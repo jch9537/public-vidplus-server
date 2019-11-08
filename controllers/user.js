3c5c4d14c0de5e40b6b32e16a12bc9160deb52d3
jch9537 <jch9537@gmail.com>
2019-11-08 11:19:32 +0900

    🎨 edit oauth_signup

--- controllers/user.js
+++ controllers/user.js
@@ -44,7 +44,6 @@ module.exports = {
           if(results === 'No match Email') return res.status(409).send({error: {status: 409, message: "일치하는 회원 정보가 존재하지 않습니다."}});
           else if(results === 'No match password') return res.status(401).send({error: {status: 401, message: "비밀번호가 일치하지 않습니다."}});
           sess.userid = results[0].id;
-          sess.oauth_signup = results[0].oauth_signup;
           res.status(200).send({ message: "Sign-in successful." });
         }
       });
@@ -113,7 +112,7 @@ module.exports = {
         if (err) res.status(500).send({error: {status: 500, message: "회원정보 가져오기 실패"}});
         else {
           console.log('겟 리절트', results)
-          res.status(200).send({email: results[0].email, name: results[0].name});
+          res.status(200).send({email: results[0].email, name: results[0].name, oauth_signup : results[0].oauth_signup});
         }
       });
     }
@@ -126,7 +125,6 @@ module.exports = {
     if(!req.session.userid) {
       res.status(401).send({error: {status: 401, message: "회원정보를 가져오기 위해 sign in이 필요합니다."}})
     } 
-    else if(req.session.oauth_signup) return res.status(303).send({message: "이 계정은 구글계정으로 가입되었습니다. 구글메일 인증을 사용해주세요."});
     else {
       const args = {
         email: data.email,
@@ -140,7 +138,7 @@ module.exports = {
       userModel.put(args, (err, results) => {
         if (err) res.status(500).send({error: {status: 500, message: "회원정보 수정 실패"}});
         else {
-          if(!results.length) return res.status(409).send({error: {status: 400, message: "다른 유저가 사용 중인 email 입니다."}}); 
+          if(!results.length) return res.status(409).send({error: {status: 409, message: "다른 유저가 사용 중인 email 입니다."}}); 
           else{
             const { email, name } = results[0];
             res.status(200).send({email, name, status: 200, message: "회원정보 수정 완료"});
